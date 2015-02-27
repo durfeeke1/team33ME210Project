f8d7ff5f79d89a3127c2fca4fc003fa4abc3f72b
Carey Hill Smith <Carey@DN0a2250ef.SUNet>
2015-02-26 18:20:49 -0800

    message

--- bumperTest/bumperTest/bumperTest.ino
+++ bumperTest/bumperTest/bumperTest.ino
@@ -1,7 +1,15 @@
-void setup() {
-Serial.begin(9600)
+int bump1 = 5;
+int bumpRead = 6;
+int val = 0;
+
+void setup(){
+Serial.begin(9600);
+pinMode(bump1, INPUT);
+pinMode(bumpRead, OUTPUT);
 }
 
-void loop() {
-
+void loop(){
+val = digitalRead(bump1);
+delay(1000);
+Serial.println(val);
 }
